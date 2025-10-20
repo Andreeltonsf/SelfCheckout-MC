d2c7c49af0c52d36f9ed6f61f08ace3659bfba5e
Francisco Ferreira <franciscoandreeltonsf@gmail.com>
2025-10-20 09:50:58 -0300

    fix: change api

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+const ProductsPage = () => {
+  return (
+    <div className="bg-black w-full h-full text-white p-8">
+      <h1>Produtos</h1>
+      <p>Lista de produtos</p>
+      <p>Lista de produtos</p>
+      <p>Lista de produtos</p>
+    </div>
+  );
+};
+
+export default ProductsPage;
